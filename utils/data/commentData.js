e194b059905e5b31c02c75907476ebedcb39e7dc
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-12-15 20:30:31 -0600

    working on comment api calls

--- utils/data/commentData.js
+++ utils/data/commentData.js
@@ -1,1 +1,25 @@
-// API calls for comments
+import { clientCredentials } from '../client';
+
+const getAllComments = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/comments`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getSingleComment = (commentId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/comments/${commentId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      resolve({
+        id: data.id,
+        postId: data.post_id,
+        authorId: data.author_id,
+        content: data.content,
+        createdOn: data.created_on,
+      });
+    })
+    .catch((error) => reject(error));
+});
+
+export { getAllComments, getSingleComment };
