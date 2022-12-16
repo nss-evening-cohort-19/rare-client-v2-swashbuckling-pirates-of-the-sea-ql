a8bcca22412c235ba9429b18ca2ae65cf366a080
NickScrimale <91919098+NickScrimale@users.noreply.github.com>
2022-12-15 20:58:18 -0600

    added forms for categories and tags

--- utils/data/categoryData.js
+++ utils/data/categoryData.js
@@ -1,1 +1,30 @@
 // API calls for categories
+/* eslint-disable import/prefer-default-export */
+export const getCategories = () => fetch('http://localhost:8088/categories')
+  .then((res) => res.json());
+
+// export const getCategoriesBySearchTerm = (searchTerm) => fetch(`http://localhost:8088/categories?search=${searchTerm}`)
+//   .then((res) => res.json());
+
+export const getCategoriedById = (id) => fetch(`http://localhost:8088/categories/${id}`)
+  .then((res) => res.json());
+
+export const addCategory = (category) => fetch('http://localhost:8088/categories', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(category),
+});
+
+export const updateCategory = (category) => fetch(`http://localhost:8088/categories/${category.id}`, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(category),
+});
+
+export const deleteCategory = (categoryId) => fetch(`http://localhost:8088/categories/${categoryId}`, {
+  method: 'DELETE',
+});
--- utils/data/tagData.js
+++ utils/data/tagData.js
@@ -1,1 +1,26 @@
 // API calls for tags
+export const getTags = () => fetch('http://localhost:8088/tags')
+  .then((res) => res.json());
+
+export const getTagsById = (id) => fetch(`http://localhost:8088/tags/${id}`)
+  .then((res) => res.json());
+
+export const addTag = (tag) => fetch('http://localhost:8088/tags', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(tag),
+});
+
+export const updateTag = (tag) => fetch(`http://localhost:8088/tags/${tag.id}`, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(tag),
+});
+
+export const deleteTag = (tagId) => fetch(`http://localhost:8088/tags/${tagId}`, {
+  method: 'DELETE',
+});
