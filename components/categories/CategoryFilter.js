de5ed126d318d3207c50e9086273af3b876fbb7c
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-12-19 21:00:53 -0600

    starting filter post by category

---
+++ components/categories/CategoryFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CategoryFilter({ catObj }) {
+  return (
+    <div>CategoryFilter</div>
+  );
+}
+
+CategoryFilter.propTypes = {};
+
+export default CategoryFilter;
