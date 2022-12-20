27dd014a5d7fa0623e5cb0de6dec0f8f1693b23a
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-12-19 20:49:18 -0600

    created postSearch component and have it rendering on main index.js, successfully searching through all posts

---
+++ components/posts/PostSearch.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+
+function PostSearch({ posts, setFilteredPosts }) {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchInput(value);
+    const results = posts.filter((post) => post.title.toLowerCase().includes(value.toLowerCase()) || post.content.toLowerCase().includes(value.toLowerCase()));
+    setFilteredPosts(results);
+  };
+
+  return (
+    <Form>
+      <Form.Control type="search" placeholder="Search Posts" value={searchInput} onChange={handleChange} />
+    </Form>
+  );
+}
+
+PostSearch.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      content: PropTypes.string,
+    }),
+  ).isRequired,
+  setFilteredPosts: PropTypes.func.isRequired,
+};
+
+export default PostSearch;
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,34 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import PostCard from '../components/posts/PostCard';
+import PostSearch from '../components/posts/PostSearch';
 import { getPosts } from '../utils/data/postData';
 
 function Home() {
+  const [filteredPosts, setFilteredPosts] = useState([]);
   const [posts, setPosts] = useState([]);
   const router = useRouter();
 
-  useEffect(() => {
-    getPosts().then(setPosts);
-  }, []);
-
   const getAllPosts = () => {
-    getPosts().then(setPosts);
+    getPosts().then((postArr) => {
+      setFilteredPosts(postArr);
+      setPosts(postArr);
+    });
   };
+
+  useEffect(() => {
+    getAllPosts();
+  }, []);
 
   return (
     <>
+      <hr />
+      <PostSearch posts={posts} setFilteredPosts={setFilteredPosts} />
+      <hr />
       <Button onClick={(() => router.push('./posts/new'))}>Add New Post</Button>
+      <hr />
       <div>
-        {posts.map((post) => (<PostCard key={post.id} createdOn={post.publication_date} title={post.title} content={post.content} imageUrl={post.image_url} userId={post.user_id} categoryId={post.category_id} id={post.id} onUpdate={getAllPosts} />))}
+        {filteredPosts.map((post) => (<PostCard key={post.id} createdOn={post.publication_date} title={post.title} content={post.content} imageUrl={post.image_url} userId={post.user_id} categoryId={post.category_id} id={post.id} onUpdate={getAllPosts} />))}
       </div>
     </>
   );
