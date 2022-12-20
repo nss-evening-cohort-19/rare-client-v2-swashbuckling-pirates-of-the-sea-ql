44cd75360bb1a95363dc4c917c85b03f0e21cdd7
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-12-19 18:57:33 -0600

    created all post components and have them rendering in appropriate pages, all API calls tested

--- components/posts/PostForm.js
+++ components/posts/PostForm.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
-import Form from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import { createPost, updatePost } from '../../utils/data/postData';
+import { getCategories } from '../../utils/data/categoryData';
 
 const initialState = {
   title: '',
   content: '',
   imageUrl: '',
-  categoryId: 0,
-
+  categoryId: null,
 };
 export default function PostForm({ postObj, user }) {
   const [currentPost, setCurrentPost] = useState(initialState);
+  const [categories, setCategories] = useState([]);
   const router = useRouter();
+
+  useEffect(() => {
+    getCategories().then(setCategories);
+    if (postObj.id) {
+      setCurrentPost(postObj);
+    }
+  }, [postObj]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,37 +34,43 @@ export default function PostForm({ postObj, user }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (postObj.id) {
-      updatePost(currentPost, user, postObj.id).then(() => router.push('/'));
+      updatePost(user, currentPost, postObj.id).then(() => router.push('/'));
     } else {
-      createPost(user, currentPost).then(() => router.push('/'));
+      createPost(currentPost, user).then(() => router.push('/'));
     }
   };
   return (
-    <Form
-      onSubmit={handleSubmit}
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '90%' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField required id="outlined-required" label="Title" name="title" onChange={handleChange} value={currentPost.title} />
-      </div>
-      <div>
-        <TextField required id="outlined-required" label="Content" name="content" onChange={handleChange} value={currentPost.content} />
-      </div>
-      <div>
-        <TextField required id="outlined-required" label="Image URL" name="imageUrl" onChange={handleChange} />
-      </div>
-      <div>
-        <TextField required id="outlined-select" select label="Select Category" name="categoryId">
-          Categories will be mapped over here
-        </TextField>
-      </div>
-      <Button variant="outlined" type="submit">SUBMIT</Button>
-    </Form>
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control name="title" required value={currentPost.title} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Content</Form.Label>
+          <textarea className="form-control" rows="5" name="content" required value={currentPost.content} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Image Url</Form.Label>
+          <Form.Control name="imageUrl" required value={currentPost.imageUrl} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Category</Form.Label>
+          <Form.Select onChange={handleChange} className="mb-3" name="categoryId" required>
+            <option value="">Select a Category</option>
+            {categories.map((category) => (
+              <option selected={category.id === currentPost.categoryId} key={category.label} value={category.id}>
+                {category.label}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 }
 
@@ -68,9 +80,18 @@ PostForm.propTypes = {
     title: PropTypes.string,
     content: PropTypes.string,
     imageUrl: PropTypes.string,
-    categoryId: PropTypes.number,
-  }).isRequired,
+    createdOn: PropTypes.string,
+    categoryId: PropTypes.shape({
+      id: PropTypes.number,
+      label: PropTypes.string,
+    }),
+  }),
   user: PropTypes.shape({
     uid: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
+};
+
+PostForm.defaultProps = {
+  postObj: initialState,
 };
--- pages/posts/edit/[postId].js
+++ pages/posts/edit/[postId].js
@@ -1,7 +1,20 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import PostForm from '../../../components/posts/PostForm';
+import { useAuth } from '../../../utils/context/authContext';
+import { getSinglePost } from '../../../utils/data/postData';
 
 export default function EditPost() {
+  const [editPost, setEditPost] = useState({});
+  const router = useRouter();
+  const { postId } = router.query;
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getSinglePost(postId).then(setEditPost);
+  }, [postId]);
+
   return (
-    <div>[postId]</div>
+    <PostForm postObj={editPost} user={user} />
   );
 }
