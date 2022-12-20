cedb4e738310a00280206955d4b6a23d372f6729
Mitch McCullough <mitchmccullough@gmail.com>
2022-12-19 20:53:07 -0600

    commiting

--- components/users/UserCard.js
+++ components/users/UserCard.js
@@ -1,15 +1,33 @@
-import { Card } from 'react-bootstrap';
+/* eslint-disable prefer-template */
+import Card from '@mui/material/Card';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import Menu from '@mui/material/Menu';
+import {
+  CardContent, CardHeader, MenuItem, Typography,
+} from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from 'react';
 import { useAuth } from '../../utils/context/authContext';
 import { deleteUser } from '../../utils/data/userData';
 import { signOut } from '../../utils/auth';
 
 export default function UserCard({ userObj }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
   const { user } = useAuth();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const deleteTheUser = () => {
     if (window.confirm('Are you sure you want to delete this?')) {
@@ -20,36 +38,75 @@ export default function UserCard({ userObj }) {
   };
 
   return (
-    <Card className="user-card">
-      <div className="user-card-left">
-        <div className="user-card-img-name">
-          <Card.Img className="user-profile-card-img" variant="top" src={userObj.profileImageUrl} />
-        </div>
-      </div>
-      <div className="user-card-right">
-        <div className="user-card-first-last">
-          <Card.Title>{userObj.firstName} {userObj.lastName}</Card.Title>
-          {user.uid === userObj.uid ? (
+    <Card sx={{ maxWidth: 500 }}>
+      {user.uid === userObj.uid ? (
+        <CardHeader
+          action={(
             <>
-              <Link className="" href={`/users/edit/${userObj.id}`} passHref>
-                <IconButton aria-label="edit" className="edit-btn">
-                  <EditIcon style={{ color: 'black' }} />
-                </IconButton>
-              </Link>
-              <IconButton aria-label="delete" className="delete-btn " onClick={deleteTheUser}>
-                <DeleteIcon style={{ color: 'black' }} />
+              <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls={open ? 'long-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <MoreVertIcon />
               </IconButton>
+              <Menu
+                id="long-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                  style: {
+                    maxHeight: 45 * 4.5,
+                    width: '20ch',
+                  },
+                }}
+              >
+                <MenuItem>
+                  <Link className="" href={`/users/edit/${userObj.id}`} passHref>
+                    <IconButton aria-label="edit" className="edit-btn verticalIcon">
+                      <EditIcon style={{ color: 'black' }} />
+                      Edit
+                    </IconButton>
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <IconButton aria-label="delete" className="delete-btn verticalIcon" onClick={deleteTheUser}>
+                    <DeleteIcon style={{ color: 'black' }} />
+                    Delete
+                  </IconButton>
+                </MenuItem>
+              </Menu>
             </>
-          ) : ('')}
-        </div>
-        <div className="user-card-email-date">
-          <Card.Text>{userObj.email}</Card.Text>
-          <Card.Text>Author Since: {userObj.createdOn}</Card.Text>
-        </div>
-        <div className="user-bio">
-          <Card.Text>{userObj.bio}</Card.Text>
-        </div>
-      </div>
+          )}
+          title={userObj.firstName + ' ' + userObj.lastName}
+          subheader={'Created on: ' + userObj.createdOn}
+        />
+      ) : (
+        <CardHeader
+          title={userObj.firstName + ' ' + userObj.lastName}
+          subheader={'Created on: ' + userObj.createdOn}
+        />
+      )}
+      <CardMedia
+        component="img"
+        height="200"
+        image={userObj.profileImageUrl}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {userObj.bio}
+        </Typography>
+        <Typography variant="body3" color="text.secondary">
+          {userObj.email}
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
