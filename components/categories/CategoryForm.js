03c4898d3f991d300aefa449c22b0dd7c1ad1149
NickScrimale <91919098+NickScrimale@users.noreply.github.com>
2022-12-20 18:14:58 -0600

    issues with edit

--- components/categories/CategoryForm.js
+++ components/categories/CategoryForm.js
@@ -1,23 +1,26 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import Button from 'react-bootstrap/Button';
+import { FloatingLabel } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
-import { addCategory, getCategories } from '../../utils/data/categoryData';
+import { addCategory, getCategories, updateCategory } from '../../utils/data/categoryData';
 
 const initialState = {
   label: '',
 };
 
 // eslint-disable-next-line react/prop-types
-function CategoryForm({ obj }) {
+function CategoryForm({ object }) {
   const [formInput, setFormInput] = useState(initialState);
   const [setCategory] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     getCategories().then(setCategory);
-    if (obj.id) setFormInput(obj);
-  }, [obj]);
+    if (object.id) setFormInput(object);
+  }, [object]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -29,43 +32,36 @@ function CategoryForm({ obj }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const payload = {
-      ...formInput,
-    };
-    addCategory(payload).then(() => {
-      setFormInput(initialState);
-    });
+    if (object.id) {
+      updateCategory(formInput)
+        .then(() => router.push('/'));
+    } else {
+      const payload = { ...formInput };
+      addCategory(payload).then(() => {
+        router.push('/');
+      });
+    }
   };
-
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3">
-        <Form.Label>Create a new category</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Add text"
-          name="label"
-          value={formInput.label}
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Create
-      </Button>
+    <Form className="form-floating" onSubmit={handleSubmit}>
+      <h2 className="text-black mt-5">{object.id ? 'Update' : 'Create'} a Category</h2>
+      <FloatingLabel controlId="floatingInput1" label="Label" className="mb-3">
+        <Form.Control type="text" placeholder="Label" name="label" value={formInput.label} onChange={handleChange} required />
+      </FloatingLabel>
+      <Button type="submit">{object.id ? 'Update' : 'Create'} Category</Button>
     </Form>
   );
 }
 
 CategoryForm.propTypes = {
-  obj: PropTypes.shape({
+  object: PropTypes.shape({
     id: PropTypes.number,
     label: PropTypes.string,
   }),
 };
 
 CategoryForm.defaultProps = {
-  obj: initialState,
+  object: initialState,
 };
 
 export default CategoryForm;
